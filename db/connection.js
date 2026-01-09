e629765a794abedd5b544046218302d0c698ae64
Adam Myers <adammyers002@gmail.com>
2026-01-09 14:32:48 -0600

    setup connection.js

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+mongoose.connection.on("error", () => {
+  console.log("MongoDB had an error connecting", error);
+});
